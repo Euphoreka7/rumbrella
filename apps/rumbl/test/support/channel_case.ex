c9824e2e7dd021bca4dd726bd6fc4c9d68fc9586
Finn Young <finnyoung88@gmail.com>
2016-05-29 17:22:37 +1200

    Authenticating a test socket.

--- apps/rumbl/test/support/channel_case.ex
+++ apps/rumbl/test/support/channel_case.ex
@@ -1,18 +1,4 @@
 defmodule Rumbl.ChannelCase do
-  @moduledoc """
-  This module defines the test case to be used by
-  channel tests.
-
-  Such tests rely on `Phoenix.ChannelTest` and also
-  imports other functionality to make it easier
-  to build and query models.
-
-  Finally, if the test case interacts with the database,
-  it cannot be async. For this reason, every test runs
-  inside a transaction which is reset at the beginning
-  of the test unless the test case is marked as async.
-  """
-
   use ExUnit.CaseTemplate
 
   using do
@@ -24,7 +10,6 @@ defmodule Rumbl.ChannelCase do
       import Ecto
       import Ecto.Changeset
       import Ecto.Query, only: [from: 1, from: 2]
-
 
       # The default endpoint for testing
       @endpoint Rumbl.Endpoint
